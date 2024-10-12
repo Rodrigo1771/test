2d3e95fbd91922b0b3031694bf854b901fbf3245
Rodrigo1771 <rmg.goncalves@campus.fct.unl.pt>
2024-10-12 11:29:35 +0100

    testing 41

--- scripts/ner/pipeline/ner_train_and_evaluate.sh
+++ scripts/ner/pipeline/ner_train_and_evaluate.sh
@@ -26,13 +26,13 @@ echo ">>> [1]   LEARNING RATE: ${LEARNING_RATE}"
 echo ">>> [1]   EPOCHS: ${EPOCHS}"
 echo ""
 
-mkdir -p "/out/${MODEL_ID#*/}/${DATASET}/"
 
 # Train model
+mkdir -p "/out/${MODEL_ID#*/}/${DATASET}/"
 python3 ner_train.py \
   --model_name_or_path "$MODEL_ID" \
   --dataset_name "$DATASET-ner" \
-  --output_dir "/out/${MODEL_ID#*/}/${DATASET}/" 2>&1 | tee "/out/${MODEL_ID#*/}/${DATASET}/train.log" \
+  --output_dir "/out/${MODEL_ID#*/}/${DATASET}/" \
   --do_train \
   --do_eval \
   --do_predict \
@@ -45,7 +45,8 @@ python3 ner_train.py \
   --load_best_model_at_end \
   --metric_for_best_model f1 \
   --disable_tqdm \
-  --seed 42
+  --seed 42 \
+   2>&1 | tee "/out/${MODEL_ID#*/}/${DATASET}/train.log"
 
 # Upload the model to Hugging Face
 BEST_MODEL_CHECKPOINT_DIR=$(python3 get_best_checkpoint_dir.py --output_dir "/out/${MODEL_ID#*/}/${DATASET}/")
